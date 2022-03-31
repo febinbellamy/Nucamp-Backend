b6905d40eae329696c2f607b8d543e86c531aacc
Febin Bellamy <febinbellamy@gmail.com>
2022-03-31 12:01:02 -0400

    Intro to Express p.2

--- node-express/server.js
+++ node-express/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3200;
 
 const app = express();
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+// Express will automatically serve index.html for GET requests to localhost:3200
 
 app.use((req, res) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
