9973ade4a45d059cbc418b4b26ad3dda0c71dcd9
Febin Bellamy <febinbellamy@gmail.com>
2022-04-05 12:57:16 -0400

    Mongoose P2 & P3

--- node-mongoose/models/campsite.js
+++ node-mongoose/models/campsite.js
@@ -1,6 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const campsiteSchema = new Schema(
   {
     name: {
@@ -11,11 +33,13 @@ const campsiteSchema = new Schema(
     description: {
       type: String,
       required: true,
-    }
+    },
+    comments: [commentSchema],
   },
   {
-    timestamps: true
-  });
+    timestamps: true,
+  }
+);
 
 const Campsite = mongoose.model("Campsite", campsiteSchema);
 
