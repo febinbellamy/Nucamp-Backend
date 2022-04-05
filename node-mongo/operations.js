af3952867cde3cb0bd6dff7aef0b5f24d8155c3a
Febin Bellamy <febinbellamy@gmail.com>
2022-04-05 10:04:29 -0400

    Node Callback Hell and Promises

--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -1,39 +1,25 @@
-const assert = require("assert").strict;
-
 // CRUD Operations
 
 // CREATE
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
   const coll = db.collection(collection);
-  coll.insertOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.insertOne(document);
 };
 
 // READ
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
   const coll = db.collection(collection);
-  coll.find().toArray((err, docs) => {
-    assert.strictEqual(err, null);
-    callback(docs);
-  });
+  return coll.find().toArray();
 };
 
 // DELETE
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
   const coll = db.collection(collection);
-  coll.deleteOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.deleteOne(document);
 };
 
 // UPDATE
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
   const coll = db.collection(collection);
-  coll.updateOne(document, { $set: update }, null, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.updateOne(document, { $set: update }, null);
 };
